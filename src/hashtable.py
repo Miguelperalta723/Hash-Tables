b5de0cc099853f4c088546b9b31e396442f878c9
Miguel Peralta <eudysperalta723@gmail.com>
2020-01-14 12:29:21 -0800

    mvp plus comments

--- src/hashtable.py
+++ src/hashtable.py
@@ -14,6 +14,7 @@ class HashTable:
     '''
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
+        self.initial_capacity = capacity
         self.storage = [None] * capacity
         self.items = 0
 
@@ -56,7 +57,24 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        #get index and item
+        index = self._hash_mod(key)
+        item = LinkedPair(key, value)
+        #if key already exist delete to update
+        if self.retrieve(key):
+            self.remove(key)
+        #if value already there add it to the linked list
+        if self.storage[index]:
+            item.next = self.storage[index]
+        #set item node to it
+        self.storage[index] = item
+        #increase items for load factor
+        self.items = self.items + 1
+        #if load factor increases past .7 resize
+        if self.load_factor() > 0.7:
+            self.resize()
+            
+       
 
 
 
@@ -68,7 +86,39 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        if self.retrieve(key) is None:
+            return
+
+        #get index
+        index = self._hash_mod(key)
+        
+        #check if item exists
+        if self.storage[index]:
+            #if first item key match change the value for the next keys value
+            item = self.storage[index]
+            if item.key == key:
+                self.storage[index] = item.next
+                self.items = self.items - 1
+            else:
+                #else loop over the list
+                while item.next is not None:
+                    # if found set pointer to one node over to remove
+                    if item.next.key == key:
+                        item.next = item.next.next
+                        self.items = self.items - 1
+                        break
+                    else:
+                        item = item.next
+        
+        if self.load_factor() < 0.2 and self.capacity > self.initial_capacity:
+            self.resize()
+
+        
+
+        
+
+
+            
 
 
     def retrieve(self, key):
@@ -79,18 +129,25 @@ class HashTable:
 
         Fill this in.
         '''
+        #initialize index as the hashed key
         index = self._hash_mod(key)
+        #check if there is a value at that index in storage
         if self.storage[index]:
+            #if there is set item to it
             item = self.storage[index]
+            #check if the keys match
             if item.key == key:
+                #if they do return value
                 return item.value
+            #else check the linked list
             else:
+                #while item.next exists check keys else set item to next
                 while item.next is not None:
                     if item.next.key == key:
                         return item.next.value
                     else:
                         item = item.next
-        
+        #return none if nothing is found
         return None
 
 
@@ -101,14 +158,32 @@ class HashTable:
 
         Fill this in.
         '''
+        #initialize a new capacity to work with
+        new_capacity = self.capacity
+        #if load factor is greater than .7 double capacity
+        if self.load_factor() > 0.7:
+            new_capacity = new_capacity * 2
+        #else if its less than 0.2 and the capacity is greater than initial one, make capicity half
+        elif self.load_factor() < 0.2 and self.capacity > self.initial_capacity:
+            new_capacity = new_capacity // 2
+        #if its still same size dont do anything
+        if new_capacity == self.capacity:
+            return
+
+        #create a new hashtable
+        new_hasttable = HashTable(new_capacity)
         
-        self.capacity *= 2
-        new_storage = [None] * self.capacity
-        for item in self.storage:
-            if item is not None:
-                new_index = self._hash_mod(item.key)
-                new_storage[new_index] = item
-        self.storage = new_storage
+        #get old hast table into new one
+        #loop over the buckets
+        for node in self.storage:
+            current_node = node
+            #loop over each bucket if there is more than one node
+            while current_node is not None:
+                new_hasttable.insert(current_node.key, current_node.value)
+                current_node = current_node.next
+        #update the capacity and storage
+        self.capacity = new_capacity
+        self.storage = new_hasttable.storage
 
 
 
